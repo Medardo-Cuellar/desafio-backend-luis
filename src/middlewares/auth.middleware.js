6fc002aba7ccc11010b89e88ee0aec31d6308081
luis rodriguez <soyriku@gmail.com>
2024-06-07 20:46:32 -0500

    fix: login router and auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,13 +1,13 @@
 const createError = require('http-errors');
 const jwt = require('../lib/jwt');
-const kodersUseCase = require('../usecases/koders.usecase');
+const usersUseCase = require('../usecases/users.usecase');
 
 async function auth(request,response,next){
     try{
         const authorization = request.headers.authorization; // el valor de aqui es un token
         if(!authorization) throw createError(401,"Authorization required"); // aqui se verifica que el token no sea falso
         const payload = jwt.verify(token);
-        const user = await kodersUseCase.getByID(payload.id);
+        const user = await usersUseCase.getByID(payload.id);
         request.user = user; // con esto estamos buscando el usuario y permitimos a los demas endpoints que accedan a la informacion de la base de datos
         next();
     }
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,16 +1,26 @@
-const jwt = require("../lib/jwt");
-const Users = require("../models/users.model"); //los modelos se importan con mayuscula
-const createError = require('http-errors');
-const encrypt = require('../lib/encrypt');
+const express = require('express');
+const router = express.Router();
+const authUseCase = require('../usecases/auth.usecase');
 
-async function login (email, password) {
-  const user = await Users.findOne({email}); // el findOne lleva un objeto con la propiedad que queremos buscar
-  if(!user) throw new createError(401,"Datos invalidos");
-  const isPasswordValid = await encrypt.compare(password, user.password);
-  if(!isPasswordValid) throw new createError(401,"Datos invalidos");
-  const token = jwt.sign({id: user._id}); // el guionbajo va en el id porque es el nombre que mongoose le da al id
-  return token;
-}
-module.exports = {
-  login
-}
+// POST /auth/login
+
+router.post('/login', async (request, response) => {
+    try {
+        const { email, password } = request.body; // no la esta pidiendo la toma de la peticion osea de la request
+        const token = await authUseCase.login(email, password);
+        response.json({
+            success: true,
+            data: {
+                token,
+            },
+        });
+        } catch (error) {
+        response.status(error.status || 500);
+        response.json({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
+module.exports = router;
